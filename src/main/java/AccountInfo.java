037abf3ccb1d9256b6ab72f0ef63404b1212d7bd
JonKappa <cappytwoj@gmail.com>
2021-07-17 15:43:48 -0400

    BankAccount changed while == false to !

---
+++ src/main/java/AccountInfo.java
@@ -0,0 +1,12 @@
+public class AccountInfo {
+    String userName;
+    String userPass;
+
+    AccountInfo(String userName, String userPass){
+        this.userName = userName;
+        this.userPass = userPass;
+
+
+
+    }
+}
